25fdcd497e6df1e29a5ede55049920cff21f1eb2
Daniel Stonier <d.stonier@gmail.com>
2016-01-09 16:22:48 +0900

    [ecl_build] comments about tricky c++11 dependencies.

--- ecl_build/cmake/ecl_cx11.cmake
+++ ecl_build/cmake/ecl_cx11.cmake
@@ -8,7 +8,23 @@ macro(ecl_check_for_cxx0x_compiler CXX0X_COMPILER_FOUND)
   CHECK_CXX_COMPILER_FLAG("-std=c++0x" CXX0X_COMPILER_FOUND)
 endmacro()
 
-
+# This is tricky, it won't be default in g++ until version 6.0
+# and in that, it will be c++14, not c++11. And this is
+# forever away.
+#
+# CMake has alot of help coming...starting in 2.8.12 and much
+# more in 3.1. The problem with using this macro below
+# directly, is that every user of a library also has to
+# enable it himself. To make the dependency transitive, i.e.
+# embed information in the library, you should add compile
+# options/features to the target.
+#
+# For examples, see what we did in ecl_linear algebra, or
+# what ceres-solver has done in 
+#
+#    https://ceres-solver.googlesource.com/ceres-solver/+/master/internal/ceres/CMakeLists.txt
+#
+# Note, there are better ways of doing this in 3.1+.
 macro(ecl_enable_cxx11_compiler)
   ecl_check_for_cxx11_compiler(CXX11_COMPILER_FOUND)
   if(CXX11_COMPILER_FOUND)
